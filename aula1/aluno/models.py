8a39d1a91c16b12a0be12c971bdc6e147b19f4fb
MatheusPMatos <mpeima@yahoo.com.br>
2024-07-26 20:27:29 -0300

    create django project

---
+++ aula1/aluno/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Alunos(models.Model):
+    nome = models.CharField(max_length=200)
+    data_nascimento = models.DateField()
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nome
+
+class Disciplina(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=6, null=True)
+
+class Professores(models.Model):
+    disciplina = models.ManyToOneRel(Disciplina, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
